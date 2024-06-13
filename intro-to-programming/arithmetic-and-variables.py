44688448a246c0583c4c8f8dfe071678f8fc079c
Nur Rohman <88269626+rohmanurnr@users.noreply.github.com>
2024-06-13 16:23:01 +0700

    Create arithmetic-and-variables.py

---
+++ intro-to-programming/arithmetic-and-variables.py
@@ -0,0 +1,40 @@
+## Printing
+print("Hello, world!\n")
+
+## Arithmetic
+print(1 + 2) #Addition
+print(9 - 5) #Subtraction
+print(2 * 4) #Multiplication
+print(6 / 3) #Division
+print(3 ** 2) #Exponent
+
+print(((1 + 3) * (9 - 2) / 2) ** 2)
+
+## Variables
+a = 4 + 5
+print(a)
+
+# Manipulation Variables
+b = 3
+print(b)
+
+b = 100
+print(b)
+print(b+3)
+
+print(a)
+print(b)
+
+## Multiple Variables
+num_years = 4
+days_per_year = 365 
+hours_per_day = 24
+mins_per_hour = 60
+secs_per_min = 60
+
+total_secs = secs_per_min * mins_per_hour * hours_per_day * days_per_year * num_years
+print(total_secs)
+
+days_per_year = 365.25
+total_secs = secs_per_min * mins_per_hour * hours_per_day * days_per_year * num_years
+print(total_secs)
